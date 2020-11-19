6dae439e17da0a9b187e8c4dc04355201dc43d06
Devon Estes <devon.c.estes@gmail.com>
2020-11-19 15:06:44 +0100

    add migration

---
+++ priv/repo/migrations/20201119140049_add_users.exs
@@ -0,0 +1,10 @@
+defmodule UserPoints.Repo.Migrations.AddUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :points, :integer
+      timestamps()
+    end
+  end
+end
