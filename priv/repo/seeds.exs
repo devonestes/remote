1bcadb7a0bcf7b37942be2afa0e5a99d1536c380
Devon Estes <devon.c.estes@gmail.com>
2020-11-19 17:33:22 +0100

    seeds file done

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,6 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+for points <- 1..100 do
+  UserPoints.Users.create_user(points)
+end
