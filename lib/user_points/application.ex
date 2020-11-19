1cae573d20b725551df89c03b738b7fc29a765a8
Devon Estes <devon.c.estes@gmail.com>
2020-11-19 15:00:16 +0100

    inital commit

---
+++ lib/user_points/application.ex
@@ -0,0 +1,34 @@
+defmodule UserPoints.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      # Start the Ecto repository
+      UserPoints.Repo,
+      # Start the Telemetry supervisor
+      UserPointsWeb.Telemetry,
+      # Start the PubSub system
+      {Phoenix.PubSub, name: UserPoints.PubSub},
+      # Start the Endpoint (http/https)
+      UserPointsWeb.Endpoint
+      # Start a worker by calling: UserPoints.Worker.start_link(arg)
+      # {UserPoints.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: UserPoints.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  def config_change(changed, _new, removed) do
+    UserPointsWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
